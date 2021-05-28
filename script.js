ba3dd26986103a9ef1acb4d223281512268e877e
Robin Joshua Del Mundo <robin.delmundo@slidegenius.com>
2021-05-29 01:00:53 +0800

    Copy Button for the Order Form

--- script.js
+++ script.js
@@ -6,21 +6,35 @@ document.addEventListener("DOMContentLoaded", startup);
 
 function add_event_listeners(){
 
-	console.log("woah");
-
+	/* float circles */
 	const circlesCount = document.querySelectorAll('.circle').length;
 	let circle;
 
-	for (let i = 0; i <= circlesCount; i++){
-
-		console.log("yo " + circle);
-
+	for (let i = 0; i < circlesCount; i++){
 		circle = document.querySelectorAll('.circle')[i];
 		circle.addEventListener("click", float_circles);
-
-
 	}
 
+	/* copy */
+	const copyButton = document.querySelector('.copy-button');
+	copyButton.addEventListener("click", copy_order_form);
+
+}
+
+function copy_order_form(){
+    const copyText = document.querySelector("#order-form");  
+    let input = document.createElement("textarea");
+
+    if(copyText){
+    	input.value = copyText.textContent;
+	    document.body.appendChild(input);
+	    input.value.trim();
+	    input.select();
+	    document.execCommand("Copy");
+	    input.remove();
+
+	    alert("Order Form Copied!")
+    }
 }
 
 function float_circles(){
