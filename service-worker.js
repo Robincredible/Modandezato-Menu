a9abe46ac240991d18fc9f6a049628f125257b84
Robin <batbord94@gmail.com>
2021-06-15 22:57:04 +0800

    Service Workers Precache Test

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,17 @@
 // Choose a cache name
-const cacheName = 'cache-v1';
+const cacheName = 'cache-v2';
 // List the files to precache
-// const precacheResources = [ '/service-worker.js', '/images', '/fonts', '/plugins', '/index.html', '/styles.css', '/script.js',
-// 'https://res.cloudinary.com/robin-optimizations/image/upload/c_fill,h_896,w_672/v1623309591/REESES_x14qbz.webp',
-// 'https://res.cloudinary.com/robin-optimizations/image/upload/c_scale,w_672/v1623304509/HERSHEYS_SPECIAL_DARK-1_r3orzh.webp',
-// 'https://res.cloudinary.com/robin-optimizations/image/upload/c_scale,w_672/v1623304509/GHIRARDELLI_WHITE-1_wpnghl.webp',
-// 'https://res.cloudinary.com/robin-optimizations/image/upload/c_fill,h_896,w_672/v1623304508/DELUXE_SAMPLER_-1_g8rzxv.webp',
-// 'https://res.cloudinary.com/robin-optimizations/image/upload/c_scale,w_672/v1623134568/BROOKIES_coyw3n.webp',
-// 'https://res.cloudinary.com/robin-optimizations/image/upload/c_crop,g_south,q_80,y_0/v1623137025/IMG_2119_zzpfps.webp',
-// 'https://res.cloudinary.com/robin-optimizations/image/upload/c_scale,q_80,w_672/v1623134569/BITE-SIZED_ue9ouf.webp',
-// 'https://res.cloudinary.com/robin-optimizations/image/upload/c_scale,q_80,w_672/v1623134568/COOKIE_CAKE_fitois.webp'];
+const precacheResources = [ '/', '/index.html', '/styles.css', '/script.js',
+'https://res.cloudinary.com/robin-optimizations/image/upload/c_fill,h_896,w_672/v1623309591/REESES_x14qbz.webp',
+'https://res.cloudinary.com/robin-optimizations/image/upload/c_scale,w_672/v1623304509/HERSHEYS_SPECIAL_DARK-1_r3orzh.webp',
+'https://res.cloudinary.com/robin-optimizations/image/upload/c_scale,w_672/v1623304509/GHIRARDELLI_WHITE-1_wpnghl.webp',
+'https://res.cloudinary.com/robin-optimizations/image/upload/c_fill,h_896,w_672/v1623304508/DELUXE_SAMPLER_-1_g8rzxv.webp',
+'https://res.cloudinary.com/robin-optimizations/image/upload/c_scale,w_672/v1623134568/BROOKIES_coyw3n.webp',
+'https://res.cloudinary.com/robin-optimizations/image/upload/c_crop,g_south,q_80,y_0/v1623137025/IMG_2119_zzpfps.webp',
+'https://res.cloudinary.com/robin-optimizations/image/upload/c_scale,q_80,w_672/v1623134569/BITE-SIZED_ue9ouf.webp',
+'https://res.cloudinary.com/robin-optimizations/image/upload/c_scale,q_80,w_672/v1623134568/COOKIE_CAKE_fitois.webp'];
 
-const precacheResources = ['/','index.html'];
+//const precacheResources = ['/','index.html'];
 
 // When the service worker is installing, open the cache and add the precache resources to it
 self.addEventListener('install', (event) => {
