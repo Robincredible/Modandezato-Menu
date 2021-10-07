ddf42f44ca11fc107e7b5eb179bc61d54402a0d2
Robin Joshua Del Mundo <robin.delmundo@slidegenius.com>
2021-10-07 11:39:42 +0800

    Service Worker update

--- service-worker.js
+++ service-worker.js
@@ -16,7 +16,6 @@ const precacheResources = [ './', './index', './styles.css', './script.js',
 // When the service worker is installing, open the cache and add the precache resources to it
 self.addEventListener('install', (event) => {
   //console.log('Service worker install event!');
-  self.skipWaiting();
   event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(precacheResources)));
 });
 
@@ -25,16 +24,16 @@ self.addEventListener('activate', (event) => {
 });
 
 // When there's an incoming fetch request, try and respond with a precached resource, otherwise fall back to the network
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((cachedResponse) => {
-      if (cachedResponse) {
-        return cachedResponse;
-      }
-      return fetch(event.request);
-    }),
-  );
-});
+// self.addEventListener('fetch', (event) => {
+//   event.respondWith(
+//     caches.match(event.request).then((cachedResponse) => {
+//       if (cachedResponse) {
+//         return cachedResponse;
+//       }
+//       return fetch(event.request);
+//     }),
+//   );
+// });
 
 addEventListener('fetch', event => {
   event.respondWith((async () => {
