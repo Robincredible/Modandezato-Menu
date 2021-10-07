5743493de1fec67592bf05c3492fc2cd228cd2b0
Robin Joshua Del Mundo <robin.delmundo@slidegenius.com>
2021-10-07 11:40:21 +0800

    Service Worker update

--- service-worker.js
+++ service-worker.js
@@ -51,36 +51,36 @@ addEventListener('fetch', event => {
 });
 
 //method 3
-// addEventListener('message', messageEvent => {
-//   if (messageEvent.data === 'skipWaiting') return skipWaiting();
-// });
+addEventListener('message', messageEvent => {
+  if (messageEvent.data === 'skipWaiting') return skipWaiting();
+});
 
-// function listenForWaitingServiceWorker(reg, callback) {
-//   function awaitStateChange() {
-//     reg.installing.addEventListener('statechange', function() {
-//       if (this.state === 'installed') callback(reg);
-//     });
-//   }
-//   if (!reg) return;
-//   if (reg.waiting) return callback(reg);
-//   if (reg.installing) awaitStateChange();
-//   reg.addEventListener('updatefound', awaitStateChange);
-// }
+function listenForWaitingServiceWorker(reg, callback) {
+  function awaitStateChange() {
+    reg.installing.addEventListener('statechange', function() {
+      if (this.state === 'installed') callback(reg);
+    });
+  }
+  if (!reg) return;
+  if (reg.waiting) return callback(reg);
+  if (reg.installing) awaitStateChange();
+  reg.addEventListener('updatefound', awaitStateChange);
+}
 
-// // reload once when the new Service Worker starts activating
-// var refreshing;
-// navigator.serviceWorker.addEventListener('controllerchange',
-//   function() {
-//     if (refreshing) return;
-//     refreshing = true;
-//     window.location.reload();
-//   }
-// );
-// function promptUserToRefresh(reg) {
-//   // this is just an example
-//   // don't use window.confirm in real life; it's terrible
-//   if (window.confirm("New version available! OK to refresh?")) {
-//     reg.waiting.postMessage('skipWaiting');
-//   }
-// }
-// listenForWaitingServiceWorker(reg, promptUserToRefresh);
+// reload once when the new Service Worker starts activating
+var refreshing;
+navigator.serviceWorker.addEventListener('controllerchange',
+  function() {
+    if (refreshing) return;
+    refreshing = true;
+    window.location.reload();
+  }
+);
+function promptUserToRefresh(reg) {
+  // this is just an example
+  // don't use window.confirm in real life; it's terrible
+  if (window.confirm("New version available! OK to refresh?")) {
+    reg.waiting.postMessage('skipWaiting');
+  }
+}
+listenForWaitingServiceWorker(reg, promptUserToRefresh);
